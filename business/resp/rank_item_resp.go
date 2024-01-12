8f021980b99e5b9429e9332474504e484ec64b89
liaozetao <1107136310@qq.com>
2024-01-12 15:46:54 +0800

    新增返回榜单标识

--- business/resp/rank_item_resp.go
+++ business/resp/rank_item_resp.go
@@ -6,6 +6,7 @@ type RankItemResp struct {
 	ObjId      int64      `json:"objId"`
 	Title      string     `json:"title"`
 	Cover      string     `json:"cover"`
+	AssetType  int8       `json:"assetType"`
 	Categories string     `json:"categories"`
 	Authors    string     `json:"authors"`
 	Score      int64      `json:"score"`
