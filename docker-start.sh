7dca97b960d212c0ebc7bb491389912d58db2f9c
liaozetao <1107136310@qq.com>
2024-01-26 09:49:01 +0800

    docker容器修改为主机模式

--- docker-start.sh
+++ docker-start.sh
@@ -16,4 +16,4 @@ mkdir -p $WORK_DIR
 
 cd $ROOT_DIR && docker build -f $ROOT_DIR/Dockerfile -t $APPLICATION_NAME --build-arg PROFILES_ACTIVE=$PROFILES_ACTIVE --build-arg APPLICATION_NAME=$APPLICATION_NAME --build-arg SERVER_PORT=$SERVER_PORT $WORK_DIR
 
-docker run --rm -p $SERVER_PORT:$SERVER_PORT --name $APPLICATION_NAME -v $ROOT_DIR:$WORK_DIR/ -v $WORK_DIR/logs:$WORK_DIR/logs -d $APPLICATION_NAME make $APPLICATION_NAME  || exit
+docker run --rm --net=host -p $SERVER_PORT:$SERVER_PORT --name $APPLICATION_NAME -v $ROOT_DIR:$WORK_DIR/ -v $WORK_DIR/logs:$WORK_DIR/logs -d $APPLICATION_NAME make $APPLICATION_NAME  || exit
