d5b0a22a164dae2b3a4ed17161a3f1492e96aa90
liaozetao <1107136310@qq.com>
2024-03-01 14:02:25 +0800

    修复缺少groupCode字段

--- basic/model/category.go
+++ basic/model/category.go
@@ -9,6 +9,7 @@ type Category struct {
 	CategoryId   int64      `json:"categoryId" xorm:"category_id pk autoincr BIGINT"`
 	CategoryCode string     `json:"categoryCode" xorm:"category_code"`
 	CategoryName string     `json:"categoryName" xorm:"category_name"`
+	GroupCode    string     `json:"groupCode" xorm:"group_code"`
 	SeqNo        int32      `json:"seqNo" xorm:"seq_no"`
 	Status       int8       `json:"status" xorm:"status"`
 	CreatedAt    types.Time `json:"createdAt" xorm:"created_at timestamp created"`
